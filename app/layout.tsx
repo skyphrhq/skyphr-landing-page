8f16d5105ceccfa3c50d74b3cf5223e07be32e70
varun <VarunSureshkumarPatel07@gmail.com>
2026-07-04 14:41:18 +0530

    chore: added the gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import NavBarComponent from "@/app/components/navbar/navBar";
 import JsonLd from "@/app/components/JsonLd";
+import NavBarComponent from "@/app/components/navbar/navBar";
+import FooterScreen from "@/app/screens/common/footerScreen";
 import "@/app/styles/globals.css";
+import { SITE_BASE_URL } from "@/app/utils/constants/common.constant";
+import { createPageMetadata } from "@/app/utils/seo/metadata";
+import { generateOrganizationSchema, generateWebsiteSchema } from "@/app/utils/seo/schema";
 import type { Metadata } from "next";
 import { Instrument_Sans, Inter, Playfair_Display } from "next/font/google";
 import SmoothScrollProvider from "./components/smoothScrollProvider";
 import { RootLayoutInterface } from "./utils/interface/common.interface";
-import FooterScreen from "@/app/screens/common/footerScreen";
-import { SITE_BASE_URL } from "@/app/utils/constants/common.constant";
-import { createPageMetadata } from "@/app/utils/seo/metadata";
-import { generateOrganizationSchema, generateWebsiteSchema } from "@/app/utils/seo/schema";
 
 const playfairDisplay = Playfair_Display({
   variable: "--font-playfair-display",
@@ -42,17 +42,55 @@ export const metadata: Metadata = {
   manifest: "/site.webmanifest",
 };
 
+import Script from "next/script";
+
 export default function RootLayout({ children }: RootLayoutInterface) {
   return (
     <html
       lang="en-US"
       dir="ltr"
       className={`${playfairDisplay.variable} ${inter.variable} ${instrumentSans.variable} antialiased`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M5ST3QB7');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+
       <body className="w-full">
+        {/* Google Tag Manager noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M5ST3QB7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager noscript */}
+
         <JsonLd data={[generateOrganizationSchema(), generateWebsiteSchema()]} />
+
         <SmoothScrollProvider>
           <NavBarComponent />
+
           {children}
+
           <footer className="w-full overflow-hidden">
             <FooterScreen />
           </footer>
