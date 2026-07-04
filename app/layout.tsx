b64a7b930d21468ccf87946359a4cc35821f5ef8
varun <VarunSureshkumarPatel07@gmail.com>
2026-07-04 14:55:43 +0530

    chore: added the gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { createPageMetadata } from "@/app/utils/seo/metadata";
 import { generateOrganizationSchema, generateWebsiteSchema } from "@/app/utils/seo/schema";
 import type { Metadata } from "next";
 import { Instrument_Sans, Inter, Playfair_Display } from "next/font/google";
+import Script from "next/script";
 import SmoothScrollProvider from "./components/smoothScrollProvider";
 import { RootLayoutInterface } from "./utils/interface/common.interface";
 
@@ -49,39 +50,43 @@ export default function RootLayout({ children }: RootLayoutInterface) {
       dir="ltr"
       className={`${playfairDisplay.variable} ${inter.variable} ${instrumentSans.variable} antialiased`}>
       <head>
-        {/* Google Tag Manager */}
-        <script>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-M5ST3QB7');
           `}
-        </script>
-
-        {/* End Google Tag Manager */}
+        </Script>
       </head>
 
       <body className="w-full">
-        {/* Google Tag Manager noscript */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-M5ST3QB7"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
           />
         </noscript>
-        {/* End Google Tag Manager noscript */}
 
         <JsonLd data={[generateOrganizationSchema(), generateWebsiteSchema()]} />
 
         <SmoothScrollProvider>
           <NavBarComponent />
-
           {children}
-
           <footer className="w-full overflow-hidden">
             <FooterScreen />
           </footer>
